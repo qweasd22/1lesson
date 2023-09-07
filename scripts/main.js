67a7ce968e261725e3f2b2ddc459b9f20f25c9f4
Даниил Кольцов <koltsov-main@mail.com>
2023-09-07 20:35:25 +0300

    adding my files to my repository

---
+++ scripts/main.js
@@ -0,0 +1,31 @@
+var myImage = document.querySelector("img");
+
+myImage.onclick = function () {
+  var mySrc = myImage.getAttribute("src");
+  if (mySrc === "image/firefox-icon.png") {
+    myImage.setAttribute("src", "image/asd2.svg");
+  } else {
+    myImage.setAttribute("src", "image/firefox-icon.png");
+  }
+};
+
+
+
+var myButton = document.querySelector("button");
+var myHeading = document.querySelector("h1");
+function setUserName() {
+    var myName = prompt("Please enter your name.");
+    localStorage.setItem("name", myName);
+    myHeading.textContent = "Mozilla is cool, " + myName;
+  }
+
+  if (!localStorage.getItem("name")) {
+    setUserName();
+  } else {
+    var storedName = localStorage.getItem("name");
+    myHeading.textContent = "Mozilla is cool, " + storedName;
+  }
+
+  myButton.onclick = function () {
+    setUserName();
+  };
